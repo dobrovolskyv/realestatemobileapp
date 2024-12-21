8078cc58f72ee72fcbea1ece17e24f3e8cde214b
Vlados <malak3@mail.com>
2024-12-21 17:39:10 +0300

    create app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {Stack} from 'expo-router';
+import {useEffect} from "react";
+import {useFonts} from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import "./global.css"
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    const [fontsLoaded] = useFonts({
+        "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+        "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+        "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+        "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+        "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+    return (
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+        <Stack/>
+
+
+    );
 }
