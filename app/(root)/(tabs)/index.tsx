7cb1239dd4346cd31eb10111764397ba20e67e59
Vlados <malak3@mail.com>
2024-12-22 18:26:37 +0300

    add profile screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,12 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 import {Link} from "expo-router";
 
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text className="font-bold my-10 font-rubik text-3xl">Welcome back</Text>
-            <Link href={"/sign-in"}>Sign in</Link>
-            <Link href={"/explore"}>Explore</Link>
-            <Link href={"/profile"}>Profile</Link>
-            <Link href={"/properties/1"}>Property</Link>
-
-        </View>
+        <SafeAreaView>
+            
+        </SafeAreaView>>
     );
 }
 
--- image.d.ts
+++ image.d.ts
@@ -1,16 +1,20 @@
 declare module '*.png' {
     const value: any;
     export default value;
-}declare module '*.jpg' {
+}
+declare module '*.jpg' {
     const value: any;
     export default value;
-}declare module '*.jpeg' {
+}
+declare module '*.jpeg' {
     const value: any;
     export default value;
-}declare module '*.gif' {
+}
+declare module '*.gif' {
     const value: any;
     export default value;
-}declare module '*.svg' {
+}
+declare module '*.svg' {
     const value: any;
     export default value;
 }
